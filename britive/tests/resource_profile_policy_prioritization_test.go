cec7e5b657e94ad37fcaf95f5ec231578b2de3e4
Yograj Shisode <yograj.shisode@britive.com>
2025-10-13 12:08:55 +0530

    Update tests for policy prioritization

--- britive/tests/resource_profile_policy_prioritization_test.go
+++ britive/tests/resource_profile_policy_prioritization_test.go
@@ -3,36 +3,33 @@ package tests
 import (
 	"fmt"
 	"testing"
-	"time"
 
 	"github.com/britive/terraform-provider-britive/britive/helpers/errs"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-func TestBritivePolicyPriority(t *testing.T) {
+func TestBritiveProfilePolicyPrioritization(t *testing.T) {
 	applicationName := "DO NOT DELETE - AWS TF Plugin"
 	profileName := "AT - New Britive Profile Policy Test"
 	profilePolicyName := "AT - New Britive Profile Policy Test"
 	profilePolicyDescription := "AT - New Britive Profile Policy Test Description"
-	timeOfAccessFrom := time.Now().AddDate(0, 0, 2).Format("2006-01-02 15:04:05")
-	timeOfAccessTo := time.Now().AddDate(0, 0, 7).Format("2006-01-02 15:04:05")
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckBritivePolicyPriorityConfig(applicationName, profileName, profilePolicyName, profilePolicyDescription, timeOfAccessFrom, timeOfAccessTo),
+				Config: testAccCheckBritiveProfilePolicyPrioritizationConfig(applicationName, profileName, profilePolicyName, profilePolicyDescription),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckBritivePolicyPriorityExists("britive_profile_policy.new"),
-					testAccCheckBritivePolicyPriorityExists("britive_profile_policy_prioritization.new_priority"),
+					testAccCheckBritiveProfilePolicyPrioritizationExists("britive_profile_policy.new"),
+					testAccCheckBritiveProfilePolicyPrioritizationExists("britive_profile_policy_prioritization.new_priority"),
 				),
 			},
 		},
 	})
 }
 
-func testAccCheckBritivePolicyPriorityConfig(applicationName, profileName, profilePolicyName, profilePolicyDescription, timeOfAccessFrom, timeOfAccessTo string) string {
+func testAccCheckBritiveProfilePolicyPrioritizationConfig(applicationName, profileName, profilePolicyName, profilePolicyDescription string) string {
 	return fmt.Sprintf(`
 	data "britive_application" "app" {
 		name = "%s"
@@ -61,70 +58,12 @@ func testAccCheckBritivePolicyPriorityConfig(applicationName, profileName, profi
 		description  = "%s"
 		profile_id   = britive_profile.new_profile.id
 		access_type  = "Allow"
-		condition    = jsonencode(
-			{
-				approval     = {
-					approvers          = {
-						tags    = [
-							"britiveProviderAcceptanceTestTag",
-							"britiveProviderAcceptanceTestTag1",
-						]
-						userIds = [
-							"britiveprovideracceptancetest",
-							"britiveprovideracceptancetest1",
-						]
-					}
-					managerApproval = {
-						condition = "All",
-						required = true
-					}
-					notificationMedium = "Email"
-					timeToApprove      = 30
-					isValidForInDays   = false
-					validFor           = 120
-				}
-				ipAddress    = "192.162.0.0/16,10.10.0.10"
-				timeOfAccess = {
-					"dateSchedule": {
-						"fromDate": "%s",
-						"toDate": "%s",
-						"timezone": "Asia/Calcutta"
-					},
-					"daysSchedule": {
-						"fromTime": "17:00:00",
-						"toTime": "17:30:00",
-						"timezone": "Asia/Calcutta",
-						"days": [
-							"FRIDAY",
-							"SATURDAY",
-							"SUNDAY"
-						]
-					}
-				}
-			}
-		)
 		consumer     = "papservice"
 		is_active    = true
 		is_draft     = false
 		is_read_only = false
 		members      = jsonencode(
 			{
-				serviceIdentities = [
-					{
-						name = "britiveProviderAcceptanceTestSI"
-					},
-					{
-						name = "britiveProviderAcceptanceTestSI1"
-					},
-				]
-				tags              = [
-					{
-						name = "britiveProviderAcceptanceTestTag"
-					},
-					{
-						name = "britiveProviderAcceptanceTestTag1"
-					},
-				]
 				users             = [
 					{
 						name = "britiveprovideracceptancetest"
@@ -135,14 +74,6 @@ func testAccCheckBritivePolicyPriorityConfig(applicationName, profileName, profi
 				]
 			}
 		)
-		associations {
-			type  = "EnvironmentGroup"
-			value = "Development"
-		}
-		associations {
-			type  = "Environment"
-			value = "Sigma Corporate"
-		}
 	}
 
 	resource "britive_profile_policy_prioritization" "new_priority" {
@@ -152,11 +83,11 @@ func testAccCheckBritivePolicyPriorityConfig(applicationName, profileName, profi
       		priority =0
     	}
 	}
-		`, applicationName, profileName, profilePolicyName, profilePolicyDescription, timeOfAccessFrom, timeOfAccessTo)
+		`, applicationName, profileName, profilePolicyName, profilePolicyDescription)
 
 }
 
-func testAccCheckBritivePolicyPriorityExists(n string) resource.TestCheckFunc {
+func testAccCheckBritiveProfilePolicyPrioritizationExists(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
 
